7ca4a863f0491a51d265b6794ee0ba6cd2a10472
Sapientia-PT <48256668+Sapientia-PT@users.noreply.github.com>
2020-08-02 20:38:35 +0100

    update

--- CoinChaser/Window.java
+++ CoinChaser/Window.java
@@ -1,6 +1,7 @@
 import java.awt.Canvas;
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Window extends Canvas{
@@ -10,10 +11,12 @@ public class Window extends Canvas{
 	
 	public Window(int width, int height, String title, Game game) {
 		JFrame frame = new JFrame(title);
+		ImageIcon img = new ImageIcon("resources/logo.png");
 		
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
+		frame.setIconImage(img.getImage());
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
